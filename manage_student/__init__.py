872beede1f5f14d57ae7430aa7a8860a23d7ca66
capchencanh <danhvip83@gmail.com>
2024-11-30 23:12:45 +0700

    fix models.py

--- manage_student/__init__.py
+++ manage_student/__init__.py
@@ -10,11 +10,11 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@localhost:3306/school_management'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'your_secret_key'
-
+__table_args__ = {'extend_existing': True}
 # Khởi tạo cơ sở dữ liệu với ứng dụng
 db = SQLAlchemy(app)
 
-# Đăng ký Blueprint
-from .routes import routes
-
-app.register_blueprint(routes)
+# # Đăng ký Blueprint
+# from .routes import routes
+#
+# app.register_blueprint(routes)
