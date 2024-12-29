2caf89e1d9f45f46e9cac719805f9cae408cbcfc
capchencanh <danhvip83@gmail.com>
2024-12-29 22:06:14 +0700

    font

--- manage_student/index.py
+++ manage_student/index.py
@@ -649,7 +649,7 @@ def export_pdf():
                 ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
                 ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
                 ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-                ('FONTNAME', (0, 0), (-1, 0), 'Roboto'),
+                ('FONTNAME', (0, 0), (-1, -1), 'Roboto'),
                 ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
                 ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
                 ('GRID', (0, 0), (-1, -1), 1, colors.black),
